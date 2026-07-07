5b6c2941a333e75540f567430aa69635e122e57d
John Jediny <John.Jediny@gsa.gov>
2026-07-07 11:58:47 -0400

    chore: remove prestart script since startup is native to bundle

--- backstage/backstage-orchestrator/prestart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-echo "Extracting Backstage production bundle..."
-tar -xzf packages/backend/dist/bundle.tar.gz
-
-echo "Starting Backstage Backend with VCAP_SERVICES bindings..."
-exec node -r ./vcap-env.js packages/backend --config app-config.yaml --config app-config.production.yaml
